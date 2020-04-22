acf9aa98638c02aee813ac42d3862b3b2e156a18
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2020-04-22 15:45:13 -0700

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,5 +86,20 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `The Project`,
+        short_name: `The Project`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `static/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
