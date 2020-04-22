9f32d65ac967c5bd14d3aa52d076da6a2bfda391
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2020-04-22 12:37:21 -0700

    updates

---
+++ src/pages/about.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <SEO
+        title="About this site"
+        description="Information about the site."
+        image="/logo.png"
+        pathname="/"
+        // Boolean indicating whether this is an article:
+        // article
+      />
+      <section>
+        <h1>About this site</h1>
+        <div>
+          <p>This is the first page I've created using Gatsby!</p>
+          <p>
+            It was originally based on the Index page, but now it's totally
+            unique!
+          </p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default AboutPage
